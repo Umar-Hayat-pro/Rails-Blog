7c13a19ea35ff37e5f7a1bd0d381665eb30210e3
Umar-Hayat-pro <umarhayatlohar0000@gmail.com>
2024-11-24 20:28:47 +0500

    Working with the controller and trying to make somethings

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,20 @@
+class BlogPostsController < ApplicationController
+  def index
+    @blog_posts = BlogPost.all
+  end
+
+  def show
+    @blog_post = BlogPost.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+  end
+
+  def new
+    @blog_post = BlogPost.new()
+  end
+
+  private
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
+end
